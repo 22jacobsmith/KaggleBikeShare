9d804df6797f113e8953ef93ddb32082af568c23
Jacob Smith <22jacobsmith@gmail.com>
2023-09-15 17:36:40 -0600

    Updated lin regression predictions

--- BikeShareAnalysisNew.R
+++ BikeShareAnalysisNew.R
@@ -18,8 +18,8 @@ bike_recipe <- recipe(count~., data=bike_train) %>%
   step_time(datetime, features="hour") %>%
   step_rm(datetime)
 bike_recipe <- prep(bike_recipe)
-train_baked <- bake(bike_recipe, new_data = bikeTrain)
-test_baked <- bake(bike_recipe, new_data = bikeTest)
+train_baked <- bake(bike_recipe, new_data = bike_train)
+test_baked <- bake(bike_recipe, new_data = bike_test)
 
 ### try a linear regression model
 
@@ -34,14 +34,18 @@ bike_workflow <- workflow() %>%
 # obtain the predictions
 bike_predictions <- predict(bike_workflow,
                             new_data = bike_test)
+
+# find mean to fill in NA values
+mean(bike_predictions[bike_predictions > 0])
 # round any negative predictions up to 0
 bike_predictions[bike_predictions < 0] <- 0
-
+bike_predictions[is.na(bike_predictions)] <- 164.4545
 
 
 # prepare the csv file
 lin_reg_output <- tibble(bike_test$datetime, bike_predictions)
 names(lin_reg_output) <- c("datetime", "count")
+
+lin_reg_output$datetime <- as.character(format(lin_reg_output$datetime))
+lin_reg_output
 write_csv(lin_reg_output, "linearRegressionPreds.csv")
-
-
